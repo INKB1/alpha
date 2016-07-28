4932ced207a2d8238a8c53dc2771002b15591c87
Vitor Nascimento <vitor.gan21@gmail.com>
2016-07-28 18:29:00 -0300

    Atualizado controles do game
    adcionado single, double e long click; obs kitkat é muito gostoso

--- Surfe de trem - Versão Alpha/Assets/_Scripts/MarkScript.cs
+++ Surfe de trem - Versão Alpha/Assets/_Scripts/MarkScript.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class MarkScript : MonoBehaviour
 {
-  Animator anim;
+    //variaveis controladoras de multiclicks
+    private float _buttonDownPhaseStart;
+    private float _doubleClickPhaseStart;
+    public float timeDoubleClick = 0.0f;
+    public float timeLongClick = 0.0f;
+
+    Animator anim;
     private Transform p_Transform;
-  private Rigidbody2D rb2d;
-  [SerializeField] private float jumpForce = 400f;
-  int jumpHash = Animator.StringToHash("Jump");
-  int beastHash = Animator.StringToHash("Beast_Jump");
-  int extremelyHash = Animator.StringToHash("Extremely_Jump");
+    private Rigidbody2D rb2d;
+    [SerializeField] private float jumpForce = 400f;
+    int jumpHash = Animator.StringToHash("Jump");
+    int beastHash = Animator.StringToHash("Beast_Jump");
+    int extremelyHash = Animator.StringToHash("Extremely_Jump");
     bool jump2 = false, jump3 = false;
     int countJump = 0;
     Renderer carga1, carga2, carga3, carga4;
@@ -28,61 +34,104 @@ public class MarkScript : MonoBehaviour
         Debug.Log(m_GroundCheck);
     }
 
-  void Start()
-  {
-    p_Transform = GetComponent<Transform>();
-    anim = GetComponent<Animator>();
-    rb2d = GetComponent<Rigidbody2D>();
-    carga1 = go1.GetComponent<Renderer>();
-    carga2 = go2.GetComponent<Renderer>();
-    carga3 = go3.GetComponent<Renderer>();
-    carga4 = go4.GetComponent<Renderer>();
-
+    void Start()
+    {
+        p_Transform = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        carga1 = go1.GetComponent<Renderer>();
+        carga2 = go2.GetComponent<Renderer>();
+        carga3 = go3.GetComponent<Renderer>();
+        carga4 = go4.GetComponent<Renderer>();
         carga1.enabled = false;
         carga2.enabled = false;
         carga3.enabled = false;
         carga4.enabled = false;
     }
 
-  void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.Space) && m_GroundCheck)
+    void Update()
     {
-      m_GroundCheck = false;
-      //Debug.Log(m_GroundCheck);
-      anim.SetTrigger (jumpHash);
-      rb2d.AddForce(new Vector2(0f, jumpForce));
+        if (Input.GetKeyDown(KeyCode.Space) && m_GroundCheck)
+        {
+            _buttonDownPhaseStart = Time.time;
+        }
+
+        if (_doubleClickPhaseStart > -1 && (Time.time - _doubleClickPhaseStart) > timeDoubleClick)
+        {
+            Debug.Log("Single Click");
+            _doubleClickPhaseStart = -1;
+            m_GroundCheck = false;
+            anim.SetTrigger(jumpHash);
+            rb2d.AddForce(new Vector2(0f, jumpForce));
             anim.Play("Jump");
-    }
-    if (Input.GetKeyDown(KeyCode.Z) && m_GroundCheck)
-    {
-      //Debug.Log(m_GroundCheck);
-      m_GroundCheck = false;
-      anim.SetTrigger (beastHash);
-      rb2d.AddForce(new Vector2(0f, jumpForce));
-            anim.Play("Beast_Jump");
-            jump2 = true;
-    }
-    if (Input.GetKeyDown(KeyCode.X) && m_GroundCheck)
-    {
-      //Debug.Log(m_GroundCheck);
-      m_GroundCheck = false;
-      anim.SetTrigger(extremelyHash);
-      rb2d.AddForce(new Vector2(0f, jumpForce));
-            anim.Play("Extremely_Jump");
-            jump3 = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && m_GroundCheck)
+        {
+            if (Time.time - _buttonDownPhaseStart > timeLongClick)
+            {
+                Debug.Log("Long Click");
+                _doubleClickPhaseStart = -1;
+                m_GroundCheck = false;
+                anim.SetTrigger(beastHash);
+                rb2d.AddForce(new Vector2(0f, jumpForce));
+                anim.Play("Beast_Jump");
+                jump2 = true;
+            }
+            else
+            {
+                if (Time.time - _doubleClickPhaseStart < timeDoubleClick)
+                {
+                    Debug.Log("Double Click");
+                    _doubleClickPhaseStart = -1;
+                    m_GroundCheck = false;
+                    anim.SetTrigger(extremelyHash);
+                    rb2d.AddForce(new Vector2(0f, jumpForce));
+                    anim.Play("Extremely_Jump");
+                    jump3 = true;
+                }
+                else
+                {
+                    _doubleClickPhaseStart = Time.time;
+                }
+            }
+            /*if (Input.GetKeyDown(KeyCode.Space) && m_GroundCheck)
+            {
+                m_GroundCheck = false;
+                //Debug.Log(m_GroundCheck);
+                anim.SetTrigger (jumpHash);
+                rb2d.AddForce(new Vector2(0f, jumpForce));
+                anim.Play("Jump");
+            }
+            if (Input.GetKeyDown(KeyCode.Z) && m_GroundCheck)
+            {
+                //Debug.Log(m_GroundCheck);
+                m_GroundCheck = false;
+                anim.SetTrigger (beastHash);
+                rb2d.AddForce(new Vector2(0f, jumpForce));
+                anim.Play("Beast_Jump");
+                jump2 = true;
+            }
+            if (Input.GetKeyDown(KeyCode.X) && m_GroundCheck)
+            {
+              //Debug.Log(m_GroundCheck);
+              m_GroundCheck = false;
+              anim.SetTrigger(extremelyHash);
+              rb2d.AddForce(new Vector2(0f, jumpForce));
+              anim.Play("Extremely_Jump");
+              jump3 = true;
+            }*/
+
+            IniciarBarraManobra();
+        }
     }
 
-        IniciarBarraManobra();
-        p_Transform.Rotate(0.0f, 0.0f, 0.0f);
-  }
-
-  void FixedUpdate()
-  {
-    anim.SetFloat("vSpeed", rb2d.velocity.y);
-    anim.ResetTrigger(jumpHash);
-    anim.ResetTrigger(beastHash);
-    anim.ResetTrigger(extremelyHash);
+    void FixedUpdate()
+    {
+        anim.SetFloat("vSpeed", rb2d.velocity.y);
+        anim.ResetTrigger(jumpHash);
+        anim.ResetTrigger(beastHash);
+        anim.ResetTrigger(extremelyHash);
         jump2 = false;
         jump3 = false;
     }
@@ -123,22 +172,4 @@ public class MarkScript : MonoBehaviour
             countJump = 0;
         }
     }
-    /*public bool CountingJumps()
-    {
-        bool jump5;
-        jump5 = false;
-        if(jump2 || jump2)
-        {
-            this.countJump++;
-        }
-
-        if(this.countJump == 5)
-        {
-            jump5 = true;
-            this.countJump = 0;
-        }
-        Debug.Log(jump5);
-        return jump5;
-
-    }*/
 }
